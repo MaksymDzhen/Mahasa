42f72bcbfd60c423e45074aa6eb5c1089087360a
MaksymDzhen <106028286+MaksymDzhen@users.noreply.github.com>
2022-05-23 14:40:12 +0300

    Update Mile-Km converter.py

--- Mile-Km converter.py
+++ Mile-Km converter.py
@@ -1,12 +1,13 @@
 print ("Hello, this is Miles/Km converter\n\n")
-
+type_1 = ["MILES", "Miles", "miles", "mls", "Mls", "m", "M", "MILE", "Mile", "mile"]
+type_2 = ["KM", "Km", "km", "K", "k"]
 type = str(input("Type what you want to convert MILES or KM: "))
-if type == "miles" or type == "MILES" or type == "Miles" or type == "mls" or type == "m":
+if type in type_1 :
     print ("OK\n")
     x = float(input("Emter miles you want to convert: \n"))
     y = x * 1.60934
     print ("It is " + str(y) + " km\n")
-elif type == "km" or type == "KM" or type == "Km" or type == "k":
+elif type in type_2:
     print("Lets do it\n")
     q = float(input("Enter km you want to convert: "))
     z = q / 1.60934
